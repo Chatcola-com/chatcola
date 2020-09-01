c2c93d5882d8d645c0c8d2e8b7b6e6176a4e69d5
anteeek <antekpapiewski@gmail.com>
2020-09-01 19:25:40 +0200

    add license headers

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,10 +1,28 @@
+/*
+|    For alternative licensing arrangements contact us at freedom@chatcola.com
+|--------------------------------------------------------------------------------  
+|    This file is part of chatcola.com server
+|    Copyright (C) 2020 Antoni Papiewski & Milan Kazarka
+|
+|    This program is free software: you can redistribute it and/or modify
+|    it under the terms of the GNU Affero General Public License as published by
+|    the Free Software Foundation, either version 3 of the License, or
+|    (at your option) any later version.
+|
+|    This program is distributed in the hope that it will be useful,
+|    but WITHOUT ANY WARRANTY; without even the implied warranty of
+|    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|    GNU Affero General Public License for more details.
+|
+|    You should have received a copy of the GNU Affero General Public License
+|    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import "reflect-metadata";
 
 import { Container } from "typedi";
 import { EventEmitter } from "events";
 
 import resolveDatabase from "../src/infrastructure/db";
-import KeyService from "../src/infrastructure/keys";
 
 Container.set("eventEmitter", new EventEmitter());
 
